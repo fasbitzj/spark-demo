f9ca8574373c699f38221c78469c8562dec2c4b2
fasbitzj <fasbit@163.com>
2017-11-06 17:29:01 +0800

    upgrade
    upgrade

--- src/test/java/com/ucl/spark/demo/SparkStream.java
+++ src/test/java/com/ucl/spark/demo/SparkStream.java
@@ -1,14 +1,17 @@
 package com.ucl.spark.demo;
 
+import kafka.security.auth.ResourceType;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
+import org.apache.spark.TaskContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka010.ConsumerStrategies;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-import org.apache.spark.streaming.kafka010.LocationStrategies;
+import org.apache.spark.streaming.kafka010.*;
+import org.spark_project.jetty.util.resource.ResourceFactory;
 import scala.Tuple2;
 
 import java.util.Arrays;
@@ -26,9 +29,10 @@ public class SparkStream {
                 kafkaParams.put("auto.offset.reset", "latest");
                 kafkaParams.put("enable.auto.commit", false);
 
-                Collection<String> topics = Arrays.asList("replication-test", "topicB");
+                Collection<String> topics = Arrays.asList("replication-threadA", "replication-threadB");
 
-                SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+//                SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+                SparkConf conf = new SparkConf().setMaster("spark://10.1.210.50:7077").setAppName("NetworkWordCount");
                 JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(1));
 
                 JavaInputDStream<ConsumerRecord<String, String>> stream =
@@ -39,5 +43,12 @@ public class SparkStream {
                                 ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
 
                 stream.mapToPair(record -> new Tuple2<>(record.key(), record.value()));
+
+                stream.foreachRDD(rdd -> {
+                        rdd.foreachPartition(consumerRecord -> {
+//                                OffsetRange o = offsetRanges[TaskContext.get().partitionId()];
+                                System.out.println("partitionId: " + TaskContext.get().partitionId());
+                        });
+                });
         }
 }
